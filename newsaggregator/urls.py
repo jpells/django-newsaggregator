8eb153f5a9e91fa28b3c574d018ca524691b9d1a
James.Pells <James.Pells@06a77226-3837-0410-8c2c-534d55b28bf2>
2008-01-14 05:31:17 +0000

    name urls for reverse lookup

--- newsaggregator/urls.py
+++ newsaggregator/urls.py
@@ -31,16 +31,16 @@ urlpatterns = patterns('',
 
 if settings.STATE_DEFAULT != settings.STATE_PUBLISHED:
     urlpatterns += patterns('django.views.generic.create_update',
-        (r'^create/$', 'create_object', dict(model=Entry, login_required=True, post_save_redirect='/news/posted/', extra_context={'STATE_DEFAULT': settings.STATE_DEFAULT})),
+        (r'^create/$', 'create_object', dict(model=Entry, login_required=True, post_save_redirect='/news/posted/', extra_context={'STATE_DEFAULT': settings.STATE_DEFAULT}), "newsaggregator_create"),
     )
 else:
     urlpatterns += patterns('django.views.generic.create_update',
-        (r'^create/$', 'create_object', dict(model=Entry, login_required=True, extra_context={'STATE_DEFAULT': settings.STATE_DEFAULT})),
+        (r'^create/$', 'create_object', dict(model=Entry, login_required=True, extra_context={'STATE_DEFAULT': settings.STATE_DEFAULT}), "newsaggregator_create"),
     )
 
 urlpatterns += patterns('django.views.generic.date_based',
     (r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/(?P<slug>[0-9A-Za-z-]+)/$', 'object_detail', dict(entry_dict_detail, slug_field='slug')),
-    (r'^$', 'archive_index', entry_dict),
+    (r'^$', 'archive_index', entry_dict, "newsaggregator_main"),
     (r'^(?P<year>\d{4})/$', 'archive_year',  entry_dict_year),
     (r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/(?P<day>\w{1,2})/$',  'archive_day', entry_dict),
     (r'^(?P<year>\d{4})/(?P<month>[a-z]{3})/$',  'archive_month', entry_dict),
